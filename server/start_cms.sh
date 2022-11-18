db35c970d2a57173685e2777d5931eae2bef69c2
Park(CMS Docker) <junhq1234@cu.ac.kr>
2022-11-18 15:32:46 +0900

    Update docker deploy script

--- server/start_cms.sh
+++ server/start_cms.sh
@@ -1,7 +1,15 @@
 IP=$(ifconfig eth0 | awk '/inet/ { print $2 }')
 echo $IP
 
+PORT=":8000"
+
+DEST=${IP}${PORT}
+
+echo $DEST
+
 cd /root/LivingLab-CCS/server
+
+pwd
 
 # Check db container connection
 while :
@@ -9,7 +17,7 @@ do
 	# Check db container connection via ping cmd
 	ping -c1 cms-main-db > /dev/null 2>&1
 	if [ $? -eq 0 ];then # if cms-main-db container connected,
-		sleep 7s
+		sleep 5s
 		python3 manage.py migrate # migrate database
 		break # and brack while loop
 	else
@@ -17,14 +25,15 @@ do
 	fi
 done
 
+sleep 5s
+
 # Create superuser
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '20121208')" | python3 manage.py shell
 
 # Add admin user to custom user
 
-
 # Start server
-python3 manage.py runserver $IP:8000
+python3 manage.py runserver $DEST
 
 # cd /root/LivingLab-CCS/server/ContentDownloadServer
 
