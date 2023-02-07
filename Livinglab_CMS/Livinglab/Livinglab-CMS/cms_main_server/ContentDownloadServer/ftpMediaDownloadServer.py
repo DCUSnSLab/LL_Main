ea898328f1258276f8578d426585f2da9c773edd
Park CMS shelter <junhq1234@cu.ac.kr>
2023-02-07 22:33:40 +0900

    Modify ftp server config

--- Livinglab_CMS/Livinglab/Livinglab-CMS/cms_main_server/ContentDownloadServer/ftpMediaDownloadServer.py
+++ Livinglab_CMS/Livinglab/Livinglab-CMS/cms_main_server/ContentDownloadServer/ftpMediaDownloadServer.py
@@ -6,7 +6,7 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
 # FTP_HOST = '203.250.33.53'
-FTP_HOST = os.environ['CMS_MAIN_IP']
+FTP_HOST = os.environ['HOST_IP']
 FTP_PORT = 9021
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -17,7 +17,8 @@ def main():
     authorizer = DummyAuthorizer()
 
     # 서버 실행 시 DB에 접속하여 쉘터 테이블에 있는 쉘터 계정을 불러와야할 듯함=
-    authorizer.add_user('shelter01', 'shelterPW', FTP_DIRECTORY, perm='elradfmwMT') #elr
+    # 현재 구현된 코드에서 크게 벗어날 수 없으므로, 우선은 shelter database 계정 명시해서 진행하겠습니다.
+    authorizer.add_user('shelter', '20121208', FTP_DIRECTORY, perm='elradfmwMT') #elr
     authorizer.add_anonymous(FTP_DIRECTORY)
 
     handler = FTPHandler
